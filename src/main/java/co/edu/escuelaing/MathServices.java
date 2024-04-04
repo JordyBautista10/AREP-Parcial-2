c7764b1fcdde5e8b5e72c86b7339cff575bd07ad
jordy.bautista <jordy.sbautista@gmail.com>
2024-04-04 14:54:01 -0500

    Linear Search

--- src/main/java/co/edu/escuelaing/MathServices.java
+++ src/main/java/co/edu/escuelaing/MathServices.java
@@ -1,12 +1,46 @@
 package co.edu.escuelaing;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static spark.Spark.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class MathServices {
 
     public static void main(String... args){
         port(getPort());
-        get("hello", (req,res) -> "Hello Docker!");
+        get("linearsearch", (req,res) -> {
+            res.type("application/json");
+            String result = "";
+            String numbers = req.queryParams("list");
+            String value = req.queryParams("value");
+            try {
+                String[] numbersList = numbers.split(",");
+                int valueInt = Integer.parseInt(value);
+                result = LinearSearch(numbersList, valueInt);
+            } catch (Exception e) {
+                result = e.getMessage();
+            }
+            Response response = new Response("linearSearch", numbers, value, result);
+            return response;
+        });
+
+        get("binarysearch", (req,res) -> {
+            String result = "";
+            String numbers = req.queryParams("list");
+            String value = req.queryParams("value");
+            try {
+                String[] numbersList = numbers.split(",");
+                int valueInt = Integer.parseInt(value);
+                result = BynarySearch(numbersList, valueInt);
+            } catch (Exception e) {
+                result = e.getMessage();
+            }
+            Response response = new Response("linearSearch", numbers, value, result);
+            return response.toString();
+        });
     }
 
     private static int getPort() {
@@ -14,6 +48,25 @@ public class MathServices {
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 4567;
+    }
+
+    private static String LinearSearch(String[] numbersList, int valueInt){
+        for (int i = 0; i < numbersList.length; i++){
+            if (Integer.parseInt(numbersList[i]) == valueInt) {
+                return String.valueOf(i);
+            }
+        }
+        return "-1";
+    }
+
+    private static String BynarySearch(String[] numbersList, int valueInt){
+        //Suponiendo que la lista esta ordenada de menor a mayor
+        for (int i = 0; i < numbersList.length; i++){
+            if (Integer.parseInt(numbersList[i]) == valueInt) {
+                return String.valueOf(i);
+            }
+        }
+        return "-1";
     }
 
 }
---
+++ src/main/java/co/edu/escuelaing/Response.java
@@ -0,0 +1,25 @@
+package co.edu.escuelaing;
+
+public class Response {
+    String operation;
+    String inputlist;
+    String value;
+    String output;
+
+    public Response(String operation, String inputlist, String value, String output){
+        this.operation = operation;
+        this.inputlist = inputlist;
+        this.value = value;
+        this.output = output;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "operation='" + operation + '\'' +
+                ", inputlist='" + inputlist + '\'' +
+                ", value='" + value + '\'' +
+                ", output='" + output + '\'' +
+                '}';
+    }
+}
